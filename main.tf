d59c80e33499131b7f2829c98860598e1b721e52
Ravisuryavanshi2 <ravisuryabanshi5@gmail.com>
2024-12-31 12:44:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,4 @@
-provider "aws" {
-  region = "ap-northeast-3"  # Set to your specified region
-}
 
-# S3 Bucket
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "hello-bucket-hosting1"  # S3 bucket name as per your specification
-}
-
-# Upload the index.html to the S3 bucket
-resource "aws_s3_object" "website_index" {
-  bucket       = aws_s3_bucket.my_bucket.bucket
-  key          = "index.html"
-  source       = "index.html"  # Path to your local index.html file
-  content_type = "text/html"
-}
-
-# EC2 Instance
 resource "aws_instance" "my_instance" {
   ami           = "ami-0206f4f885421736f"  # Your specified AMI ID
   instance_type = "t2.micro"  # Adjust based on your needs
@@ -27,11 +10,38 @@ resource "aws_instance" "my_instance" {
   }
 }
 
+provider "aws" {
+  region = "ap-northeast-3"  # Set to your specified region
+}
 
+# S3 Bucket
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "hello-bucket-hosting1"  # S3 bucket name
+}
+
+# Upload the index.html to the S3 bucket
+resource "aws_s3_object" "website_index" {
+  bucket       = aws_s3_bucket.my_bucket.bucket
+  key          = "index.html"
+  source       = "index.html"  # Path to your local index.html file
+  content_type = "text/html"
+}
+
+# CloudFront Origin Access Control (OAC)
+resource "aws_cloudfront_origin_access_control" "my_oac" {
+  name                              = "hello-oac"
+  description                       = "OAC for CloudFront to access S3 bucket"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+# CloudFront Distribution
 resource "aws_cloudfront_distribution" "my_distribution" {
   origin {
-    domain_name = aws_s3_bucket.my_bucket.bucket_regional_domain_name
-    origin_id   = "S3-hello-bucket-hosting1"
+    domain_name             = aws_s3_bucket.my_bucket.bucket_regional_domain_name
+    origin_id               = "S3-hello-bucket-hosting1"
+    origin_access_control_id = aws_cloudfront_origin_access_control.my_oac.id
   }
 
   # Default cache behavior configuration
@@ -40,33 +50,57 @@ resource "aws_cloudfront_distribution" "my_distribution" {
     viewer_protocol_policy = "redirect-to-https"
     compress               = true
 
-    allowed_methods = ["GET", "HEAD"]  # Allowed HTTP methods
-    cached_methods  = ["GET", "HEAD"]  # Cached HTTP methods
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods  = ["GET", "HEAD"]
 
     forwarded_values {
-      query_string = false   # You can set this to true if you want to forward query strings
+      query_string = false
       cookies {
-        forward = "none"      # Options: "none", "all", or "whitelist"
+        forward = "none"
       }
-      headers = []            # If you want to forward specific headers, you can list them here
     }
   }
 
   enabled             = true
   is_ipv6_enabled     = true
   comment             = "My CloudFront Distribution"
-  price_class         = "PriceClass_100"  # You can choose different pricing tiers
+  price_class         = "PriceClass_100"
   default_root_object = "index.html"
-  
+
   # Viewer certificate
   viewer_certificate {
-    cloudfront_default_certificate = true  # Using default CloudFront certificate for HTTPS
+    cloudfront_default_certificate = true
   }
 
-  # Restrictions (Optional but required in some cases)
+  # Restrictions
   restrictions {
     geo_restriction {
-      restriction_type = "none"  # No geographic restrictions
+      restriction_type = "none"
     }
   }
+}
+
+# Automatically Attach Bucket Policy
+resource "aws_s3_bucket_policy" "my_bucket_policy" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "AllowCloudFrontAccessWithOAC"
+        Effect    = "Allow"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.my_bucket.arn}/*"
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.my_distribution.arn
+          }
+        }
+      }
+    ]
+  })
 }
